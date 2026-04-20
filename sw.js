144b25a74403a02aecc798b32eb1792b3c27ac23
dondolladev <williambrew5@gmail.com>
2026-04-20 10:19:23 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = "nolimit-budget-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) {
+        fetch(e.request).then(res => {
+          if (res && res.ok) {
+            caches.open(CACHE).then(c => c.put(e.request, res.clone())).catch(() => {});
+          }
+        }).catch(() => {});
+        return cached;
+      }
+      return fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone)).catch(() => {});
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
